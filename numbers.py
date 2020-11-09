df13ee4ff076276fc2e5807283a456b7912e12ea
Mario J <mjere003@gmail.com>
2020-11-09 10:59:49 -0500

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -4,6 +4,7 @@ from string import ascii_uppercase
 
 flag =[]
 
+"""The numbers included in the numbers list below are the flag that needs to be deciphered."""
 numbers = [16, 9, 3, 15, 3, 20, 6, '{', 20, 8, 5, 14, 21, 13, 2, 5, 18, 19, 13, 1, 19, 15, 14, '}']
 
 for number in numbers:
